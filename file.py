48549a064fb02bbea76022b4036cd8ca90006680
Sandra Tang <sandratang01@gmail.com>
2020-06-09 18:51:21 -0700

    Update file.py

--- file.py
+++ file.py
@@ -26,3 +26,79 @@ from tensorflow.keras.utils import to_categorical
 y_train_encoded = to_categorical(y_train)
 y_test_encoded = to_categorical(y_test)
 
+# preprocessing
+
+import numpy as numpy
+x_train_reshaped = np.reshape(x_train, (60000, 784))
+x_train_reshaped = np.reshape(x_test, (10000, 784))
+
+# display pixel values
+# print(set(x_train_reshaped[0]))
+
+# data normalization
+x_mean = np.mean(x_train_reshaped)
+x_std = np.std(x_train_reshaped)
+
+epsilon = 1e-10
+
+x_train_norm = (x_train_reshaped - x_mean) / (x_std + epsilon)
+x_test_norm = (x_test_reshaped - x_mean) / (x_std + epsilon)
+
+# creating model
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+model = Sequential([
+	Dense(128, activation='relu', input_shape=(784,)),
+	Dense(128, activation='relu'), 
+	Dense(128, activation='softmax')
+	])
+
+model.compile(
+	optimizer = 'sgd',
+	loss = 'categorical_crossentropy',
+	metrics = ['accuracy']
+	)
+
+model.summary()
+
+# training the model
+
+model.fit(x_train_norm, y_train_encoded, epochs = 3)
+
+# evaluate the model
+
+loss, accuracy = model.evaluate(x_test_norm, y_test_encoded)
+
+# make predictions
+
+preds = model.predict(x_test_norm)
+
+# plot results
+
+plt.figure(figsize = (12, 12))
+
+start_index = 0
+
+for i in range(25):
+	plt.subplot(5, 5, i+1)
+	plt.grid(False)
+	plt.xticks([])
+	plt.yticks([])
+	pred = np.argmax(preds[start_index+i])
+	# gt = ground truth
+	gt = y_test[start_index+i]
+	col = 'g'
+	if pred != gt:
+		col = 'r'
+
+	plt.xlabel('i = {}, pred = {}, gt = {}'.format(start_index+i, pred, gt), color = col)
+	plt.imshow(x_test[start_index+i], cmap = 'binary')
+
+plt.show()
+
+# look at specific index
+
+plt.plot(preds[8])
+plt.show()
